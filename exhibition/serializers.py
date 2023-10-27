f217fb7f4672298154dab11a69d7806a0f96b291
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2023-10-28 01:16:36 +0900

    create serializers.py

---
+++ exhibition/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import Year, Team, Member, Info, Photo
+
+class YearSerializer(ModelSerializer):
+    class Meta:
+        model = Year
+        fields = ('year')
+
+class TeamSerializer(ModelSerializer):
+    year = YearSerializer()
+    class Meta:
+        model = Team
+        fields = ('year', 'team', 'p_name')
+
+class MemberSerializer(ModelSerializer):
+    team = TeamSerializer()
+    class Meta:
+        model = Member
+        fields = ('team','name','m_id','major')
+
+class InfoSerializer(ModelSerializer):
+    team = TeamSerializer()
+    madeby = MemberSerializer()
+    class Meta:
+        model = Info
+        fields = ('team','madeby')
+
+class PhotoSerializer(ModelSerializer):
+    team_id = TeamSerializer()
+    class Meta:
+        model = Photo
+        fields = ('team_id', 'index', 'photo')
