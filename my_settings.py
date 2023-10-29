df4644dcf835c6fdde63422d3c0d73d75f74d0f3
SonSuBin129 <ttokky0129@gmail.com>
2023-10-29 20:49:06 +0900

    views.py 4차

--- my_settings.py
+++ my_settings.py
@@ -6,6 +6,7 @@ DATABASES = {
         'PASSWORD': '$321RecruiT!#%&(',
         'HOST': '118.67.135.185',
         'PORT': '3306',
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
 SECRET_KEY = 'django-insecure-n4o-+oaj6nueh5lol16sdw#^9ekb72xt2@pv5k#r5+_vk#f3uw'
